aac29f294688335b360faaa13553a6ba374b6153
Bacilio Alexander Bolaños Lima <alima@creditsesame.com>
2022-10-17 11:53:48 -0600

    Feature: Added Dockerfile, moved the source files to folder src

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:alpine
+WORKDIR /usr/app/src
+COPY package*.json ./
+
+RUN npm install --production
+COPY src .
+
+CMD ["node", "server.js"]
--- operations/index.js
+++
@@ -1,7 +0,0 @@
-const sumar = (a, b) => {
-
-    if(typeof a !== 'number' || typeof b !== 'number') return null;
-    return  parseFloat(a)+parseFloat(b);
-}
-
-exports.sumar = sumar;
